667e763e51a0a8f0e373040e5a97cd37374518cd
MarveQ <evgenij.brukin@ukr.net>
2023-07-18 12:55:29 -0400

    commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const session = require('express-session')
+
+const FileStore = require('session-file-store') (session)
+const passport = require('passport')
+
+const app = express()
+const port = 3001
+
+app.use(express.json()) // to recognize the incoming Request Object as a JSON Object.
+app.use(express.urlencoded({extended: false})) //to recognize the incoming Request Object as strings or arrays
+
+
+app.use(
+    session({
+        secret: 'awfwagt',
+        store: new FileStore(),
+        cookie: {
+            path: '/',
+            httpOnly: true,
+            maxAge: 60 * 60 * 1000
+        },
+        resave: false,
+        saveUninitialized: false
+    })
+)
+
+
+require('./passportConfig') // подключение конфига
+
+app.use(passport.initialize()) //подключаемся к сессии
+app.use(passport.session()) //сохраняем пользоватеся
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+//сюда отправляем email и password для аунтификации
+app.post('/login', (req, res, next) => {
+    passport.authenticate("local", function (err, user){
+        if(err) {
+            return next(err);
+        }
+        if(!user) {
+            return res.send('Enter correct email and password');
+        }
+        req.login(user, function(err) {
+            if(err){
+                return next(err);
+            }
+            return res.redirect('/admin');
+        })
+    }) (req, res, next); //?
+})
+
+
+const auth = (req, res, next) => { //?
+    return res.redirect('/');
+}
+
+
+app.get('/admin', auth, (req, res) => {
+    res.send('Admin Page')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ passportConfig.js
@@ -0,0 +1,31 @@
+const passport = require("passport");
+
+const LocalStrategy = require("passport-local")
+const user = {
+    id: 1,
+    email: "test@gmail.com",
+    password: "1234"
+}
+
+passport.serializeUser(function (user, done) { // сохраняет user
+        console.log("Serialization ", user);
+        done(null, user.id);                            //только id?
+    }
+)
+
+passport.deserializeUser(function (id, done) { // проверяет id
+        console.log("Deserialization", id)
+        const user1 = user.id === id ? user : false; //сравниваем id юзера из database(user) с id который вернул serialize
+        done(null, user1);
+    }
+)
+
+passport.use( //?
+    new LocalStrategy({usernameField: 'email'}, {passwordField: 'password'}, function verify(email, password, done) {
+        if(email === user.email && password === user.password){
+            return done(null, user)
+        }else {
+            return done(null, false)
+        }
+    })
+)
